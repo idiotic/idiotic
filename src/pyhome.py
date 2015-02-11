fa00e7f5bbd85c843e5d69cf17ae8412e72b8415
Dylan Whichard <dylan@whichard.com>
2015-02-10 19:10:43 -0500

    Reorganize main

--- src/pyhome.py
+++ src/pyhome.py
@@ -23,9 +23,34 @@ import os
 import sys
 import docopt
 import logging
-from pyhome import utils, item
+import threading
+import schedule
+import time
+from pyhome import utils, item, items, rule, dispatcher, _scheduler_thread
 
-def main():
+class ShutdownWaiter:
+    def __init__(self):
+        self.running = True
+        self.threads = []
+
+    def stop(self):
+        self.running = False
+
+    def join_all(self):
+        self.stop()
+        for thread in self.threads:
+            thread.join()
+
+    def started(self, thread):
+        self.threads.append(thread)
+
+    def __bool__(self):
+        return self.running
+
+waiter = None
+
+def init():
+    global waiter
     # load command-line options
     arguments = docopt.docopt(__doc__, version="Current version")
 
@@ -34,24 +59,43 @@ def main():
     # A little strange, but this should work correctly for everything.
     arguments = {k: os.path.join(arguments["base"],v.replace("<base>/","")) if type(v) is str and "<base>" in v else v for k,v in arguments.items()}
 
-    print(arguments)
-
-    logging.basicConfig(level=max(0, 5-arguments["verbose"]))
-    logging.info("Loading items from {}".format(arguments["items"]))
-    items = utils.load_dir(arguments["items"])
-    item.load(items)
-    print(items)
     # load configuration
-    # load bindings
+    logging.basicConfig(level=max(0, 5-arguments["verbose"]))
+
     # load items
+    logging.info("Loading items from {}".format(arguments["items"]))
+    utils.load_dir(arguments["items"])
+
+    # load bindings
     # load rules
+    logging.info("Loading rules from {}".format(arguments["rules"]))
+    utils.load_dir(arguments["rules"])
     # load ui
     # read database
     # correspond with other instances?
     # start running updates / bindings
+    # Our non-daemon threads will wait on this
+    waiter = ShutdownWaiter()
+
+    schedule_thread = threading.Thread(target=_scheduler_thread, daemon=True)
+    schedule_thread.start()
+
     # start running rules
     # start serving API
     # start serving UI
 
+    # cleanup stuff!
+
+def shutdown():
+    waiter.join_all()
+    logging.shutdown()
+
 if __name__ == '__main__':
-    main()
+    init()
+
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print("Shutting down")
+        shutdown()
