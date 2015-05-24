a17d048359627420bd018175e4f9161f235c8ceb
Dylan Whichard <dylan@whichard.com>
2015-05-23 20:20:28 -0400

    asyncio-ify dispatch

--- src/idiotic/dispatch.py
+++ src/idiotic/dispatch.py
@@ -1,11 +1,14 @@
 from idiotic.event import EventFilter
+from asyncio import coroutine, Queue, sleep
 import logging
+import functools
 
 log = logging.getLogger("idiotic.dispatch")
 
 class Dispatcher:
     def __init__(self):
         self.bindings = []
+        self.queue = Queue()
 
     def bind(self, action, filt=EventFilter()):
         # The default event filter will always return True
@@ -19,4 +22,15 @@ class Dispatcher:
 
     def dispatch(self, event):
         for action in (a for a, f in self.bindings if f.check(event)):
-            action(event)
+            log.debug("Doing {}".format(str(action)))
+
+            self.queue.put(functools.partial(action, event))
+            log.debug("Done")
+
+    @coroutine
+    def run(self):
+        while True:
+            while self.queue.empty():
+                yield from asyncio.sleep(.01)
+
+            yield from self.queue.get()
