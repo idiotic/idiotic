30a4a31815febd4f8a3eec161567a9dbebdc09cd
Dylan Whichard <dylan@whichard.com>
2015-06-09 21:26:43 -0400

    Add support for directory modules

--- src/idiotic/utils.py
+++ src/idiotic/utils.py
@@ -5,19 +5,27 @@ import logging
 
 def load_dir(path):
     sys.path.insert(0, os.path.abspath("."))
-    files = []
-    for _, _, f in os.walk(path):
-        files.extend(f)
-        break
-
     modules = []
-    for f in files:
+    for f in os.listdir(path):
         try:
-            if f.startswith(".") or f.endswith("~") or f.endswith("#"):
+            if f.startswith(".") or f.endswith("~") or f.endswith("#") or f.startswith("__"):
                 continue
+
             logging.info("Loading file {}...".format(os.path.join(path, f)))
             name = os.path.splitext(f)[0]
-            modules.append(imp.load_source(name, os.path.join(path, f)))
+
+            try:
+                modules.append(imp.load_source(name, os.path.join(path, f)))
+            except IsADirectoryError:
+                logging.info("Attempting to load directory {} as a module...".format(
+                    os.path.join(path, f)))
+
+                try:
+                    modules.append(imp.load_source(name, os.path.join(path, f, '__init__.py')))
+                except FileNotFoundError:
+                    logging.error("Unable to load module {}: {} does not exist".format(
+                        name, os.path.join(path, f, '__init__.py')))
         except Exception as e:
             logging.exception("Exception encountered while loading {}".format(os.path.join(path, f)))
+
     return modules
