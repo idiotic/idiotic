bb851db35cbd9588c329f3634de051788d30e91f
Dylan Whichard <dylan@whichard.com>
2015-08-03 00:17:28 -0400

    Add docstrings to base persistence

--- src/idiotic/persistence.py
+++ src/idiotic/persistence.py
@@ -20,27 +20,39 @@ class Persistence(metaclass=PersistenceType):
         self.disconnect()
 
     def connect(self):
+        """Establish a connection to the database, if necessary."""
         pass
 
     def disconnect(self):
+        """Close the connection to the database, if necessary."""
         pass
 
     def create(self):
+        """Create and initialize the database, if it does not already exist.
+May be called automatically by the persistence engine if needed.
+
+        """
         pass
 
     def version(self):
+        """The version of the database when it was created or last updated.
+"""
         pass
 
     def upgrade(self, old_version, new_version):
+        """Convert the database from an old version to a new version."""
         pass
 
     def sync(self):
+        """Write any uncommitted data to the underlying database engine."""
         pass
 
     def purge(self):
+        """Delete any data that does not meet retention requirements."""
         pass
 
     def get_item_history(self, item, kind="state", since=None, count=-1):
+        """Retrieve the history for the given item."""
         pass
 
     def append_item_history(self, item, time, value, kind="state", extra=None):
