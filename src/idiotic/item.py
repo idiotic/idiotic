690da7840d7ffb2cdf3437543027d2ae65de171f
Dylan Whichard <dylan@whichard.com>
2015-10-25 02:13:12 -0400

    Stop spammy state non-change info messages

--- src/idiotic/item.py
+++ src/idiotic/item.py
@@ -200,7 +200,8 @@ class BaseItem:
             LOG.debug("Ignoring redundant state change for {}".format(self))
             return
 
-        LOG.info("{} changed state from {} -> {}".format(self, self._state, val))
+        if self._state != val:
+            LOG.info("{} changed state from {} -> {}".format(self, self._state, val))
         old = self._state
         pre_event = event.StateChangeEvent(self, old, val, source, kind="before")
         idiotic.dispatcher.dispatch(pre_event)
