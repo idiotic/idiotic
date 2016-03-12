3c1ccd4ae8db29d76aa3aa1473fa540220e73529
Dylan Whichard <dylan@whichard.com>
2016-03-12 11:00:39 -0500

    Change idiotic module to Idiotic object

--- idiotic/__init__.py
+++ idiotic/__init__.py
@@ -11,35 +11,6 @@ from .dispatch import Dispatcher
 
 LOG = logging.getLogger("idiotic.init")
 
-config = {}
-
-name = "idiotic"
-
-items = TaggedDict()
-
-scenes = TaggedDict()
-
-modules = AttrDict()
-
-_distribs = AttrDict()
-
-_persistences = AttrDict()
-
-scheduler = schedule.Scheduler()
-
-dispatcher = Dispatcher()
-
-persist_instance = None
-
-distribution = None
-distrib_thread = None
-
-_root_api = Flask(__name__)
-
-_apis = {}
-
-api = None
-
 # Monkeypatch schedule so nobody has to deal with it directly
 def __sched_job_do_once(self, func, *args, **kwargs):
     self.do(lambda: func(*args, **kwargs) and schedule.CancelJob or schedule.CancelJob)
@@ -51,150 +22,169 @@ def __sched_job_do_now(self, func, *args, **kwargs):
 schedule.Job.do_once = __sched_job_do_once
 schedule.Job.do_now = __sched_job_do_now
 
-@asyncio.coroutine
-def run_scheduled_jobs():
-    while True:
-        try:
-            runnable_jobs = sorted((job for job in scheduler.jobs if job.should_run))
-            if len(runnable_jobs):
-                for job in runnable_jobs:
-                    # i'm sorry this is kind of terrible
-                    # but dammit, it works
-                    ret = yield from asyncio.coroutine(job.job_func)()
+class Idiotic:
+    def __init__(self, config=None, name="idiotic"):
+        if config is None:
+            config = {}
+        self.config = config
+        self.name = name
 
-                    while asyncio.iscoroutine(ret):
-                        ret = yield from ret
+        self.items = TaggedDict()
+        self.scenes = TaggedDict()
+        self.modules = AttrDict()
+        self._distribs = AttrDict()
+        self._persistences = AttrDict()
+        self.scheduler = schedule.Scheduler()
+        self.dispatcher = Dispatcher()
+        self.persist_instance = None
+        self.distribution = None
+        self.distrib_thread = None
+        self._root_api = Flask(__name__)
+        self._apis = {}
+        self.api = None
 
-                    job.last_run = datetime.datetime.now()
-                    job._schedule_next_run()
+    @asyncio.coroutine
+    def run_scheduled_jobs():
+        while True:
+            try:
+                runnable_jobs = sorted((job for job in self.scheduler.jobs if job.should_run))
+                if len(runnable_jobs):
+                    for job in runnable_jobs:
+                        # i'm sorry this is kind of terrible
+                        # but dammit, it works
+                        ret = yield from asyncio.coroutine(job.job_func)()
 
-                    if isinstance(ret, schedule.CancelJob):
-                        scheduler.cancel_job(job)
-            else:
+                        while asyncio.iscoroutine(ret):
+                            ret = yield from ret
+
+                        job.last_run = datetime.datetime.now()
+                        job._schedule_next_run()
+
+                        if isinstance(ret, schedule.CancelJob):
+                            self.scheduler.cancel_job(job)
+                else:
+                    yield from asyncio.sleep(1)
+            except:
+                LOG.exception("Exception in scheduler.run_pending()")
                 yield from asyncio.sleep(1)
-        except:
-            LOG.exception("Exception in scheduler.run_pending()")
-            yield from asyncio.sleep(1)
 
-def _set_config(conf):
-    config.update(conf)
+    def _set_config(conf):
+        config.update(conf)
 
-def _register_item(item):
-    items._set(item.name, item)
+    def _register_item(item):
+        self.items._set(item.name, item)
 
-def _register_scene(name, scene):
-    scenes._set(name, scene)
+    def _register_scene(name, scene):
+        self.scenes._set(name, scene)
 
-def _register_distrib(name, distrib):
-    _distribs._set(name, distrib)
+    def _register_distrib(name, distrib):
+        self._distribs._set(name, distrib)
 
-def _register_persistence(name, cls):
-    _persistences._set(name, cls)
+    def _register_persistence(name, cls):
+        self._persistences._set(name, cls)
 
-def _register_module(module, assets=None):
-    name = mangle_name(getattr(module, "MODULE_NAME", module.__name__))
+    def _register_module(module, assets=None):
+        name = mangle_name(getattr(module, "MODULE_NAME", module.__name__))
 
-    mod_conf = config.get("modules", {}).get(name, {})
+        mod_conf = config.get("modules", {}).get(name, {})
 
-    if mod_conf.get("disable", False):
-        LOG.info("Module {} is disabled; skipping registration".format(name))
-        return
+        if mod_conf.get("disable", False):
+            LOG.info("Module {} is disabled; skipping registration".format(name))
+            return
 
-    base = mod_conf.get("api_base", "/api/module/" + name)
+        base = mod_conf.get("api_base", "/api/module/" + name)
 
-    if base == '/':
-        mod_api = _root_api
-    elif base in _apis:
-        mod_api = _apis[base]
-    else:
-        mod_api = Flask(name)
+        if base == '/':
+            mod_api = self._root_api
+        elif base in self._apis:
+            mod_api = self._apis[base]
+        else:
+            mod_api = Flask(name)
 
-        _apis[mod_conf.get("api_base", "/api/module/" + name)] = mod_api
+        self._apis[mod_conf.get("api_base", "/api/module/" + name)] = mod_api
 
-    if hasattr(module, "configure"):
-        LOG.info("Configuring module {}".format(name))
-        module.configure(
-            mod_conf,
-            _APIWrapper(mod_api, module, '/'),
-            assets
-        )
+        if hasattr(module, "configure"):
+            LOG.info("Configuring module {}".format(name))
+            module.configure(
+                mod_conf,
+                _APIWrapper(mod_api, module, '/'),
+                assets
+            )
 
-    if hasattr(module, "start"):
-        LOG.info("Starting module {}".format(name))
-        asyncio.get_event_loop().call_soon(module.start)
+        if hasattr(module, "start"):
+            LOG.info("Starting module {}".format(name))
+            asyncio.get_event_loop().call_soon(module.start)
 
-    modules._set(name, module)
+        self.modules._set(name, module)
 
-def _finalize_api():
-    global api
-    api = DispatcherMiddleware(_root_api, _apis)
+    def _finalize_self():
+        self.api = DispatcherMiddleware(self._rootapi, self.apis)
 
-    return api
+        return self.api
 
-def _register_builtin_module(module, assets=None):
-    name = mangle_name(getattr(module, "MODULE_NAME", module.__name__))
+    def _register_builtin_module(module, assets=None):
+        name = mangle_name(getattr(module, "MODULE_NAME", module.__name__))
 
-    if hasattr(module, "configure"):
-        LOG.info("Configuring system module {}".format(name))
-        module.configure(
-            config,
-            config.get(name, {}),
-            _APIWrapper(_root_api, module, '/'),
-            assets
-        )
+        if hasattr(module, "configure"):
+            LOG.info("Configuring system module {}".format(name))
+            module.configure(
+                config,
+                config.get(name, {}),
+                _APIWrapper(self._root_api, module, '/'),
+                assets
+            )
 
-    modules._set(name, module)
+        self.modules._set(name, module)
 
-def _send_event(evt):
-    LOG.debug("_send_event!")
-    distribution.send(event.pack_event(evt))
+    def _send_event(evt):
+        LOG.debug("_send_event!")
+        self.distribution.send(event.pack_event(evt))
 
-def _recv_event(evt):
-    LOG.debug("_recv_event!")
-    dispatcher.dispatch(event.unpack_event(evt, modules))
+    def _recv_event(evt):
+        LOG.debug("_recv_event!")
+        self.dispatcher.dispatch(event.unpack_event(evt, self.modules))
 
-def _start_distrib(dist, host, conf):
-    global distribution, distrib_thread
+    def _start_distrib(dist, host, conf):
+        try:
+            dist_cls = self._distribs[dist]
+        except NameError as e:
+            raise NameError("Could not find self.distribution method {}".format(dist), e)
+        self.distribution = dist_cls(host, conf)
+        self.distribution.connect()
 
-    try:
-        dist_cls = _distribs[dist]
-    except NameError as e:
-        raise NameError("Could not find distribution method {}".format(dist), e)
-    distribution = dist_cls(host, conf)
-    distribution.connect()
+        self.dispatcher.bind(_send_event, utils.Filter(not_hasattr='_remote'))
+        self.distribution.receive(_recv_event)
 
-    dispatcher.bind(_send_event, utils.Filter(not_hasattr='_remote'))
-    distribution.receive(_recv_event)
+        thread = threading.Thread(target=self.distribution.run, daemon=True)
+        thread.start()
 
-    thread = threading.Thread(target=distribution.run, daemon=True)
-    thread.start()
+    def _stop_distrib():
+        if self.distribution:
+            self.distribution.disconnect()
 
-def _stop_distrib():
-    if distribution:
-        distribution.disconnect()
+        if self.distrib_thread:
+            self.distrib_thread.join()
 
-    if distrib_thread:
-        distrib_thread.join()
+    def _record_state_change(evt):
+        if evt and evt.item:
+            self.persist_instance.append_item_history(evt.item, evt.time, evt.new, kind="state")
 
-def _record_state_change(evt):
-    if evt and evt.item:
-        persist_instance.append_item_history(evt.item, evt.time, evt.new, kind="state")
+    def _start_persistence(persist, conf):
+        persist_cls = self._persistences[persist]
+        self.persist_instance = persist_cls(conf)
+        self.persist_instance.connect()
+        self.dispatcher.bind(_record_state_change, utils.Filter(type=event.StateChangeEvent, kind="after"))
 
-def _start_persistence(persist, conf):
-    global persist_instance
-    persist_cls = _persistences[persist]
-    persist_instance = persist_cls(conf)
-    persist_instance.connect()
-    dispatcher.bind(_record_state_change, utils.Filter(type=event.StateChangeEvent, kind="after"))
-    for item in items.all():
-        history = list(persist_instance.get_item_history(item))
-        if len(history):
-            item._state = history[-1][0]
-        if hasattr(item, "state_history"):
-            for state in history:
-                item.state_history.record(*state)
+        for item in self.items.all():
+            history = list(self.persist_instance.get_item_history(item))
+            if len(history):
+                item._state = history[-1][0]
 
-def _stop_persistence():
-    if persist_instance:
-        persist_instance.sync()
-        persist_instance.disconnect()
+            if hasattr(item, "state_history"):
+                for state in history:
+                    item.state_history.record(*state)
+
+    def _stop_persistence():
+        if self.persist_instance:
+            self.persist_instance.sync()
+            self.persist_instance.disconnect()
