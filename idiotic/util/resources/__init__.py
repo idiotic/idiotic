83b90c0f3548b5d30b53508bbe0f0d1a1130d19e
Dylan Whichard <dylan@whichard.com>
2016-10-01 15:20:07 -0400

    Add a basic host-is resource

---
+++ idiotic/util/resources/__init__.py
@@ -0,0 +1,14 @@
+from idiotic.resource import Resource
+from idiotic.config import config
+
+
+class NodeIs(Resource):
+    def __init__(self, node):
+        self.node = node
+
+    @property
+    def available(self):
+        return self.node == config.hostname()
+
+    async def available_hosts(self, config):
+        return [self.node]
