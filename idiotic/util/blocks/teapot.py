187efbc4dd2249742426ba03d35a9e837a92bf59
Dylan Whichard <dylan@whichard.com>
2016-10-02 04:23:52 -0400

    Switch kettle stuff to aiohttp

--- idiotic/util/blocks/teapot.py
+++ idiotic/util/blocks/teapot.py
@@ -3,7 +3,9 @@ from idiotic import block
 from idiotic import resource
 from idiotic import node
 import asyncio
+import aiohttp
 import time
+
 
 class TeapotBlock(block.Block):
     def __init__(self, name, config):
@@ -22,18 +24,28 @@ class TeapotBlock(block.Block):
         self.hold_start = 0
         self.hold_duration = 0
 
-    def temperature(self, value):
+    async def temperature(self, value):
         print("setting temp to {}".format(value))
-        requests.get("{}{}{}/set_temp".format(self.config['address'], self.config['path'], self.config['device_id']), data={'access_token': self.config['access_token'], 'args': str(value)})
+        async with aiohttp.ClientSession() as client:
+            async with client.post(
+                    "{}{}{}/set_temp".format(self.config['address'], self.config['path'], self.config['device_id']),
+                    data={'access_token': self.config['access_token'], 'args': str(value)}
+            ) as request:
+                print(await request.text())
 
-    def hold(self, value):
+    async def hold(self, value):
         print("holding for {}".format(value))
         self.hold_start = time.time()
         self.hold_duration = value
 
     async def run(self):
         while await self.check_resources():
-            await asyncio.sleep(20)
-            if (time.time() - self.hold_duration) < self.hold_start:
-                requests.get("{}{}{}/set_hold".format(self.config['address'], self.config['path'], self.config['device_id']), data={'access_token': self.config['access_token'], 'args': str(30)})
+            if True or (time.time() - self.hold_duration) < self.hold_start:
+                async with aiohttp.ClientSession() as client:
+                    async with client.post(
+                            "{}{}{}/set_hold".format(self.config['address'], self.config['path'], self.config['device_id']),
+                            data={'access_token': self.config['access_token'], 'args': str(30)}
+                    ) as request:
+                        print(await request.text())
+            await asyncio.sleep(5)
         node.cluster.assign_block(self)
