adc83f71a1588adb0a8e19df81604ee99dad3620
Mark Murnane <mark@hackafe.net>
2016-10-01 22:56:51 -0400

    Made teapot more reasonable

--- idiotic/util/blocks/teapot.py
+++ idiotic/util/blocks/teapot.py
@@ -3,15 +3,30 @@ from idiotic import block
 from idiotic import resource
 
 class TeapotBlock(block.Block):
-    def __init__(self):
+    def __init__(self, config={}, global_config={}):
+        self.config = {"address": "https://api.particle.io",
+                       "path": "/v1/devices/",
+                       "access_token": "",
+                       "device_id": ""
+                      }
+        self.config.update(config)
+
         self.inputs = {"temperature": block.Input(),
                        "hold": block.Input()
                       }
-        self.resources = [resource.PingResource(config['address'])]
+        self.resources = [resource.PingResource(self.config['address'])]
         self.connect(**self.inputs)
+        self.hold_start = 0
+        self.hold_duration = 0
 
     def temperature(self, value):
-        requests.get("{}{}{}/set_temp".format(config('address', 'https://api.particle.io'), config('path', '/v1/devices/'), config('device_id')), data={'access_token': config('access_token'), 'args': str(value)})
+        requests.get("{}{}{}/set_temp".format(self.config['address'], self.config['path'], self.config['device_id']), data={'access_token': self.config['access_token'], 'args': str(value)})
 
     def hold(self, value):
-        requests.get("{}{}{}/set_hold".format(config('address', 'https://api.particle.io'), config('path', '/v1/devices/'), config('device_id')), data={'access_token': config('access_token'), 'args': str(value)})
+        self.hold_start = time.time()
+        self.hold_duration = value
+
+    async def run(self):
+        await asyncio.sleep(20)
+        if (time.time() - self.hold_duration) < self.hold_start:
+            requests.get("{}{}{}/set_hold".format(self.config['address'], self.config['path'], self.config['device_id']), data={'access_token': self.config['access_token'], 'args': str(30)})
