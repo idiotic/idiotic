afcbd58189b9357106efca0e6dc7d18cb78b7f45
Dylan Whichard <dylan@whichard.com>
2016-10-02 07:34:06 -0400

    pysyncobj whyyy

--- idiotic/cluster.py
+++ idiotic/cluster.py
@@ -18,16 +18,17 @@ class UnassignableBlock(Exception):
 
 
 class Cluster(pysyncobj.SyncObj):
+    block_owners = {}
+    block_lock = RLock()
+    resources = {}
+    jobs = {}
+
     def __init__(self, configuration: config.Config):
         super(Cluster, self).__init__(
             '{}:{}'.format(configuration.hostname, configuration.cluster['port']),
             [h for h in configuration.cluster['connect'] if not h.startswith(configuration.hostname + ':')]
         )
         self.config = configuration
-        self.block_owners = {}
-        self.block_lock = RLock()
-        self.resources = {}
-        self.jobs = []
 
     async def find_destinations(self, event):
         return self.config.nodes.keys()
