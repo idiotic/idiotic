1e153d306ecc14e9c078731c12c7b037eb687de3
Mark Murnane <mark@hackafe.net>
2016-10-02 05:47:35 -0400

    Added additional random types

--- idiotic/util/blocks/random.py
+++ idiotic/util/blocks/random.py
@@ -5,7 +5,7 @@ from idiotic import resource
 from idiotic import node
 
 
-class RandomBlock(block.Block):
+class RandomFloatBlock(block.Block):
     def __init__(self, name, config):
         self.name = name
         self.config = {"period": 1,
@@ -20,5 +20,32 @@ class RandomBlock(block.Block):
     async def run(self):
         await asyncio.sleep(self.config['period'])
         val = random.random()*(self.config['max']-self.config['min'])+self.config['min']
-        print("Setting random value of {}".format(val))
+        await self.output(val)
+
+class RandomBoolBlock(block.Block):
+    def __init__(self, name, config):
+        self.name = name
+        self.config = {"period": 1
+                      }
+        self.config.update(config)
+        self.resources = []
+
+    async def run(self):
+        await asyncio.sleep(self.config['period'])
+        val = bool(random.getrandbits(1))
+        await self.output(val)
+
+class RandomIntBlock(block.Block):
+    def __init__(self, name, config):
+        self.name = name
+        self.config = {"period": 1,
+                       "min": 0,
+                       "max": 1
+                      }
+        self.config.update(config)
+        self.resources = []
+
+    async def run(self):
+        await asyncio.sleep(self.config['period'])
+        val = random.randrange(self.config['min'], self.config['max'])
         await self.output(val)
