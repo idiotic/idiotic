eb7687b1f5042e7960bdd7f25e9a925abbf8a0fc
Dylan Whichard <dylan@whichard.com>
2016-10-13 00:57:38 -0400

    Add random list block

--- idiotic/util/blocks/random.py
+++ idiotic/util/blocks/random.py
@@ -49,3 +49,14 @@ class RandomIntBlock(block.Block):
         await asyncio.sleep(self.config['period'])
         val = random.randrange(self.config['min'], self.config['max'])
         await self.output(val)
+
+
+class RandomList(block.Block):
+    def __init__(self, name, period=1, items=None):
+        super().__init__(name)
+        self.period = period
+        self.items = items or []
+
+    async def run(self):
+        await asyncio.sleep(self.period)
+        await self.output(random.choice(self.items))
