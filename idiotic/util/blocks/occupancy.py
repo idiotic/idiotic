587b35796921b0c81e5f261775b0311feb504eab
Dylan Whichard <dylan@whichard.com>
2018-02-11 16:26:04 -0500

    Add staged occupancy

--- idiotic/util/blocks/occupancy.py
+++ idiotic/util/blocks/occupancy.py
@@ -12,6 +12,81 @@ OPEN = False
 
 NULL_EVENT = Event(None, None, 0)
 
+EPSILON = 1e-3
+
+
+class StagedMotion(block.Block):
+    # The first value is the "cooldown" period -- for this many seconds after the initial motion
+    #     event, additional motion events will not trigger a stage upgrade
+    # The second value is the activation period -- after the cooldown period ends, any motion event
+    #     for this many seconds will trigger the next stage.
+    # Once the last stage has been reached, if a motion event occurs after the
+    DEFAULT_STAGES = [
+        (5, 20),
+        (30, 60),
+        (300, 1800),
+    ]
+
+    def __init__(self, *args, stages=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if not stages:
+            stages = self.DEFAULT_STAGES
+
+        self.stages = stages
+        self.cur_stage = None
+        self.last_trigger = None
+
+    def __getattr__(self, key):
+        if key.startswith("motion_"):
+            async def __motion(val):
+                # We only care about when motion starts
+                if not val:
+                    return
+
+                now = time.time()
+
+                if self.cur_stage is None:
+                    # First time!
+                    self.cur_stage = 0
+                    self.last_trigger = now
+
+                    await self.output(True)
+                else:
+                    if now - self.last_trigger > self.stages[self.cur_stage][0]:
+                        # Not in the cooldown state anymore
+                        self.last_trigger = now
+
+                        # Move to the next stage if we can
+                        if self.cur_stage < len(self.stages) - 1:
+                            self.cur_stage += 1
+
+            return __motion
+        else:
+            raise ValueError("Parameter not declared")
+
+    async def _recalculate(self):
+        if self.cur_stage is not None:
+            stage_info = self.stages[self.cur_stage]
+            if self.last_trigger + stage_info[0] + stage_info[1] <= time.time():
+                # The timeout has expired
+                self.cur_stage = None
+                await self.output(False)
+
+    def get_recalc_time(self):
+        if self.cur_stage is not None:
+            stage_info = self.stages[self.cur_stage]
+            return self.last_trigger + stage_info[0] + stage_info[1]
+        else:
+            # The earliest we would need to recalculate is if we immediately get a motion event,
+            # and then no more and it expires.
+            return time.time() + sum(self.stages[0])
+
+    async def run(self):
+        while True:
+            await asyncio.sleep(self.get_recalc_time() - time.time())
+            await self._recalculate()
+
 
 class Occupancy(block.Block):
     def __init__(self, *args, threshold=.45, decay=.85, motion=None, doors=None, sound=None, **kwargs):
